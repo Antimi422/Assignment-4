f1cfb001e7a1002f10621a6fd2dde9ba08e18c33
Antimi422 <tonyfeng422@yahoo.ca>
2024-04-08 12:23:59 -0600

    did some menu

--- class-assignment-4/Program.cs
+++ class-assignment-4/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Clients;
+
+
+bool loopAgain = true;
+while (loopAgain)
+{
+    try
+    {
+        DisplayMainMenu();
+        string mainMenuChoice = Prompt("\nEnter a Main Menu Choice: ").ToUpper();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+
+void DisplayMainMenu()
+{
+    Console.WriteLine("\n/------------------------------/");
+    Console.WriteLine("     Personal Training App      ");
+    Console.WriteLine("/------------------------------/");
+    Console.WriteLine("");
+    Console.WriteLine("\nMenu Options");
+    Console.WriteLine("===============");
+    Console.WriteLine("[N]ew client");
+    Console.WriteLine("[S]how client BMI info");
+    Console.WriteLine("[E]dit client");
+    Console.WriteLine("[Q]uit");
+}
+
+string Prompt(string prompt)
+{
+    string myString = "";
+    while (true)
+    {
+        try
+        {
+            Console.Write(prompt);
+            myString = Console.ReadLine().Trim();
+            if (string.IsNullOrEmpty(myString))
+                throw new Exception($"Empty Input: Please enter something.");
+            break;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    return myString;
+}
